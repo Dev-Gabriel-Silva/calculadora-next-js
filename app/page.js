860937a28a35343769e77c6c5ebdde520ce5d09a
Gabriel <dev.gabriel2000@gmail.com>
2025-11-13 08:44:30 -0300

    style: atualizar cores dos botões da calculadora

--- app/page.js
+++ app/page.js
@@ -20,23 +20,23 @@ export default function Home() {
         <div
           className={`grid grid-cols-4 gap-2 m-4 w-2xl text-black`}
         >
-          <Button className={`col-span-3 bg-gray-400`}>AC</Button>
-          <Button className={`bg-gray-400`}>/</Button>
+          <Button className={`col-span-3 bg-red-600`}>AC</Button>
+          <Button className={`bg-green-600`}>/</Button>
           <Button>7</Button>
           <Button>8</Button>
           <Button>9</Button>
-          <Button className={`bg-gray-400`}>x</Button>
+          <Button className={`bg-green-600`}>x</Button>
           <Button>4</Button>
           <Button>5</Button>
           <Button>6</Button>
-          <Button className={`bg-gray-400`}>-</Button>
+          <Button className={`bg-green-600`}>-</Button>
           <Button>1</Button>
           <Button>2</Button>
           <Button>3</Button>
-          <Button className={`bg-gray-400`}>+</Button>
+          <Button className={`bg-green-600`}>+</Button>
           <Button className={`col-span-2`}>0</Button>
           <Button>.</Button>
-          <Button className={`bg-gray-400`}>=</Button>
+          <Button className={`bg-green-600`}>=</Button>
         </div>
       </section>
     </main>
